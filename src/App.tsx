71a8193551a5eced282f21abfc03dd5a3c92af29
Silvestre Esteban Fernández Liñero <silvestref@missionreadyhq.com>
2024-08-30 18:03:11 +1200

    created a node application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useState } from 'react';
 import React from 'react';
+import 'App.css';
 
 const App: React.FC = () => {
   return (
-      <div>Hello, World!</div>
+      <div className='parent-div-ai-text-chat'>
+        <div className='ai-text-chat-outer-shell'>
+          <div className='ai-text-chat-inner-shell'>
+            <div className='ai-text-chat-form'>
+
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
