fb8bce0dba3a16b179a2921eb9382fb30f6c16ee
Silvestre Esteban Fernández Liñero <silvestref@missionreadyhq.com>
2024-09-02 20:40:36 +1200

    trying to get it running on azure

--- src/Ai-text-chat.tsx
+++ src/Ai-text-chat.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import JobTitle from './JobTitle';
 const API_KEY = import.meta.env.VITE_GEMINI_KEY;
 
 
-const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel( { 
-    model: "gemini-1.5-flash",
-     systemInstruction: "You are a job interviewer, you are interviewing a candidate for a software engineering position. The candidate has done a tech accelerator program, and has experience with HTML, CSS, JS, Node.js, typescript, React.js, and Vite.js. Keep your responses brief. There will be 2 questions and 2 answers. After that I want you give the user a review on how well they answered the questions, and suggest how their responses could be improved." });
+
 
 const AiTextChat: React.FC = () => {
     const [chatText, setChatText] = useState<string>('');
     const [conversation, setConversation] = useState<{ text: string, role: 'user' | 'interviewer' }[]>([
         {text: 'Interviewer: Tell me about yourself.', role: 'interviewer'}
     ]);
+
+
+    const [systemInstruction, setSystemInstruction] = useState('You are a job interviewer, you are interviewing a candidate for a software engineering position. The candidate has done a tech accelerator program, and has experience with HTML, CSS, JS, Node.js, typescript, React.js, and Vite.js. Keep your responses brief. There will be 2 questions and 2 answers. After that I want you give the user a review on how well they answered the questions, and suggest how their responses could be improved.')
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel( { 
+    model: "gemini-1.5-flash",
+    systemInstruction: systemInstruction
+    });    
+
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         if (chatText.trim() !== '') {
@@ -29,6 +34,23 @@ const AiTextChat: React.FC = () => {
         }
     };
 
+    const [jobTitle, setJobTitle] = useState('');
+    const handleKeyPress = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            alert(`You entered ${jobTitle}`);
+            if (jobTitle === 'Junior Software Developer') {
+              setSystemInstruction('You are a job interviewer, you are interviewing a candidate for a software engineering position. The candidate has done a tech accelerator program, and has experience with HTML, CSS, JS, Node.js, typescript, React.js, and Vite.js. Keep your responses less than 50 words. Get straight into the interview on the first interaction.');
+            } else if (jobTitle === 'UX Designer') {
+                setSystemInstruction('You are a job interviewer, you are interviewing a candidate for a UX design position. The candidate has done a tech accelerator program, and has experience with Figma, Adobe XD, and Sketch. Keep your responses less than 50 words. Get straight into the interview on the first interaction. Remember that this is an interview, I am applying for a job.');
+            } else if (jobTitle === 'Building Apprentice') {
+                setSystemInstruction('You are a job interviewer, you are interviewing somebody who has many years of building/carpentry experience and is doing his apprenticeship. Keep your responses less than 50 words. Get straight into the interview on the first interaction. Remember that this is an interview, I am applying for a job.');
+            }
+
+           
+        }
+    };
+
     return (
         <div className='parent-div-ai-text-chat'>
           <div className='ai-text-chat-outer-shell'>
@@ -37,7 +59,17 @@ const AiTextChat: React.FC = () => {
                 
                     <h3>AI Mock Interviewer</h3>
                     <br />
-                    <JobTitle />
+                    <div>
+            <span id='job-title-span'><label htmlFor="job-title-input">Job Title:</label>
+            <input  type="text"
+                    id="job-title-input"
+                    name="job-title-input"
+                    value={jobTitle}
+                    onChange={(e) => setJobTitle(e.target.value)}
+                    onKeyDown={handleKeyPress}
+                    required/></span>
+            <br />
+        </div>
                     <div id='conversation-display' className='conversation-display'>
                             {conversation.map((msg, index) => (
                                 <p key={index} className={msg.role === 'user' ? 'user-text' : 'interviewer-text'}>
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import React from 'react';
 import './App.css';
 import AiTextChat from './Ai-text-chat';
